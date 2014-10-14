7e419efa69962af5ec54467e730e8cbd9b087cba
Federico Tomassetti <f.tomassetti@gmail.com>
2014-10-14 18:47:46 +0100

    Creating Civs.Simulation module

--- src/Civs/Civs.hs
+++ src/Civs/Civs.hs
@@ -3,20 +3,12 @@
 
 import Civs.Model
 import Civs.Pickle
-import Civs.Graphics
+import Civs.Simulation
 
-import qualified Data.List as L
-import Data.Word
 import Data.Map
-import Data.Char
 import qualified Data.ByteString.Lazy as S
-import Data.Typeable
-import Codec.Picture
 import System.Random
-import System.Console.ANSI
-import System.IO
 import Civs.ConsoleExplorer
-import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 
@@ -50,51 +42,6 @@ generateGame world seed ngroups = helper g0 ss0 ngroups
                                         ss0 = randoms rg :: [Int]
                                         helper g ss 0 = g
                                         helper g (s:ss) n = helper (generateGroup g s) ss (n-1)
-
-simulation :: (TVar Game) -> (MVar ()) -> IO ()
-simulation syncGame syncScreen = do
-    rg <- newStdGen
-    let ss = randoms rg :: [Int]
-    simLoop syncGame syncScreen ss
-
-type RandomIntSeq = [Int]
-
-data Event = NoEvent
-             | NewGroup
-             | NewSettlement Int
-             deriving Eq
-
-instance Show Event where
-    show NoEvent = "<nothing>"
-    show NewGroup = "Creating new group"
-    show (NewSettlement id) = "Creating new settlement for group " ++ (show id)
-
-simEvent :: RandomIntSeq -> (TVar Game) -> IO (Event, RandomIntSeq)
-simEvent randomSeq syncGame = do
-    game <- atomRead syncGame
-    return $ case randomValue' of
-        0 -> (NoEvent, tail randomSeq)
-        1 -> (NewGroup, tail randomSeq)
-        2 -> let groups = gameGroups game
-             in (NewSettlement $ groupId (head groups), tail randomSeq)
-    where randomValue = head randomSeq
-          randomValue' = randomValue `mod` 3
-
-executeEvent syncGame NoEvent = return ()
-
-executeEvent syncGame NewGroup = return ()
-
-executeEvent syncGame (NewSettlement groupId) = return ()
-
-simLoop :: (TVar Game) -> (MVar ()) -> RandomIntSeq -> IO ()
-simLoop syncGame syncScreen randomInts = do
-    takeMVar syncScreen
-    (event,randomInts') <- simEvent randomInts syncGame
-    executeEvent syncGame event
-    drawNews $ " "++ (show event)
-    putMVar syncScreen ()
-    threadDelay 1000000
-    simLoop syncGame syncScreen randomInts'
 
 initialGame worldFileName = do
     byteString <- S.readFile worldFileName :: IO S.ByteString
---
+++ src/Civs/Simulation.hs
@@ -0,0 +1,54 @@
+module Civs.Simulation where
+
+import Civs.Model
+
+import System.Random
+import Civs.ConsoleExplorer
+import Control.Concurrent
+import Control.Concurrent.STM
+
+simulation :: (TVar Game) -> (MVar ()) -> IO ()
+simulation syncGame syncScreen = do
+    rg <- newStdGen
+    let ss = randoms rg :: [Int]
+    simLoop syncGame syncScreen ss
+
+type RandomIntSeq = [Int]
+
+data Event = NoEvent
+             | NewGroup
+             | NewSettlement Int
+             deriving Eq
+
+instance Show Event where
+    show NoEvent = "<nothing>"
+    show NewGroup = "Creating new group"
+    show (NewSettlement id) = "Creating new settlement for group " ++ (show id)
+
+simEvent :: RandomIntSeq -> (TVar Game) -> IO (Event, RandomIntSeq)
+simEvent randomSeq syncGame = do
+    game <- atomRead syncGame
+    return $ case randomValue' of
+        0 -> (NoEvent, tail randomSeq)
+        1 -> (NewGroup, tail randomSeq)
+        2 -> let groups = gameGroups game
+             in (NewSettlement $ groupId (head groups), tail randomSeq)
+    where randomValue = head randomSeq
+          randomValue' = randomValue `mod` 3
+
+executeEvent syncGame NoEvent = return ()
+
+executeEvent syncGame NewGroup = return ()
+
+executeEvent syncGame (NewSettlement groupId) = return ()
+
+simLoop :: (TVar Game) -> (MVar ()) -> RandomIntSeq -> IO ()
+simLoop syncGame syncScreen randomInts = do
+    takeMVar syncScreen
+    (event,randomInts') <- simEvent randomInts syncGame
+    executeEvent syncGame event
+    drawNews $ " "++ (show event)
+    putMVar syncScreen ()
+    threadDelay 1000000
+    simLoop syncGame syncScreen randomInts'
+
